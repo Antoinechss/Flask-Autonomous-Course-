a8171d858d33f17ce88c52ef6205cbd5f40e6096
Antoinechss <antoine.chosson@eleves.enpc.fr>
2025-11-02 17:23:12 +0100

    commit

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,9 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+import sqlalchemy as sa 
+from app import db 
+from app.models import User 
 
 class LoginForm(FlaskForm):
     username = StringField("username", validators=[DataRequired()])
     password = PasswordField("password", validators=[DataRequired()])
     remember_me = BooleanField("Remember me")
     submit = SubmitField("sign in")
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("password", validators=[DataRequired()])
+    password2 = PasswordField("repeat password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        # check in database if username already exists 
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
+        if user is not None : 
+            raise ValidationError("Username already in use")
+        
+    def validate_email(self, email): 
+        # check in database if email already exists 
+        user = db.session.scalar(sa.select(User).where(User.email == email.data))
+        if user is not None : 
+            raise ValidationError("Email already in use")
+
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,11 +1,16 @@
-from app import app
-from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+from app import app, db
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required 
+import sqlalchemy as sa
+from app.models import User
+from urllib.parse import urlsplit
+from datetime import datetime, timezone
 
 @app.route("/")
 @app.route("/index")
+#@login_required
 def index():
-    user = {"username": "Antoine"}
     posts = [
         {"author": {"username": "Laurence"},
          "body": "Beautiful day in France today",
@@ -14,12 +19,59 @@ def index():
          "body": "I am in Tokyo now",
          "date": "02-09-2025"}
     ]
-    return render_template("index.html", title="home", user=user, posts=posts)
+    return render_template("index.html", title="home", posts=posts)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))        
+    # check if current user is already authenticated
+    if current_user.is_authenticated:
         return redirect(url_for("index"))
-    return render_template("/login.html", title="Sign in", form=form)
+    form = LoginForm()
+    # If the form is correctly filled in : 
+    if form.validate_on_submit():
+        # fetching infos in database, look for match
+        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+        # If no existing user or password not matching
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password") 
+            return redirect(url_for("login"))   
+        # If username and password match  
+        login_user(user, remember=form.remember_me.data) 
+        next_page = request.args.get("next") 
+        if not next_page or urlsplit(next_page).netloc != "":
+            next_page = url_for("index")
+        return redirect(next_page)
+    return render_template("login.html", title="Sign in", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Successfully registered !")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
+
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [{'author': user, 'body': 'Test post #1'},
+            {'author': user, 'body': 'Test post #2'}]
+    return render_template("user.html", user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
